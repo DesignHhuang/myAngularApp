dff2fd8cfefb89a1929316f0b31f6f6f46dc7ef9
huangxiaomin <1219549841@qq.com>
2017-11-24 16:59:50 +0800

    12321

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { CoreModule } from './core/core.module'
+import { MdSidenavModule } from '@angular/material'
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    CoreModule,
+    MdSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,25 @@
+import { NgModule, SkipSelf, Optional } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { MdToolbarModule } from '@angular/material'
+@NgModule({
+  imports: [
+    CommonModule,
+    MdToolbarModule,
+  ],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
+  exports: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+  ]
+})
+export class CoreModule {
+  constructor( @Optional() @SkipSelf() parent: CoreModule) {
+    if (parent) {
+      throw new Error('模块已经存在，不能加载！');
+    }
+  }
+}
